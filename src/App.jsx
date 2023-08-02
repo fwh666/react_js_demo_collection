a28bd4a4a660b83da40c3f342d550a25480e3df4
ext.fuwenhao3 <ext.fuwenhao3@jd.com>
2023-08-02 18:28:38 +0800

    add: base

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,8 @@
 import React, { Component } from 'react'
-import {Link,Route, Routes} from 'react-router-dom'
-import Home from './components/Home'
-import About from './components/About'
-
 export default class App extends Component {
 	render() {
-		return (
-			<div>
-				<div className="row">
-					<div className="col-xs-offset-2 col-xs-8">
-						<div className="page-header"><h2>React Router Demo</h2></div>
-					</div>
-				</div>
-				<div className="row">
-					<div className="col-xs-2 col-xs-offset-2">
-						<div className="list-group">
-
-							{/* 原生html中，靠<a>跳转不同的页面 */}
-							{/* <a className="list-group-item" href="./about.html">About</a>
-							<a className="list-group-item active" href="./home.html">Home</a> */}
-
-							{/* 在React中靠路由链接实现切换组件--编写路由链接 */}
-							<Link className="list-group-item" to="/about">About</Link>
-							<Link className="list-group-item" to="/home">Home</Link>
-						</div>
-					</div>
-					<span>===============</span>
-					<div className="col-xs-6">
-						<div className="panel">
-							<div className="panel-body">
-								{/* 注册路由 */}
-								{/* <Routes>
-									<Route path="/" element={<HomePage />} />
-									<Route path="/about" component={About}/>
-									<Route path="/home" component={Home}/>
-								</Routes> */}
-								<Routes>
-									<Route path='/about' element={<About/>}>about</Route>
-									<Route path='/home' element={<Home/>}>home</Route>
-								</Routes>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+		return(
+			<div>sss</div>
 		)
 	}
 }
