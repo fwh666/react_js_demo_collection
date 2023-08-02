175419b5d62c916c27e970375957a52474518963
ext.fuwenhao3 <ext.fuwenhao3@jd.com>
2023-08-02 16:46:14 +0800

    add: github&search

---
+++ src/components/List/index.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import './index.css'
+/**
+ * 根据出参解析字段数据
+ * {
+        login: "yyx990803",
+        html_url: "https://github.com/yyx990803",
+        avatar_url:
+          "https://avatars3.githubusercontent.com/u/499550?s=460&u=de41ec9325e8a92e281b96a1514a0fd1cd81ad4a&v=4",
+        id: 1,
+      },
+ */
+
+
+export default class List extends Component {
+    render() {
+        const {users,isFirst,isLoading,err} = this.props
+        return (
+            <div className="row">
+                {
+                    isFirst ? <h2>欢迎使用，输入关键字，随后点击搜索</h2> :
+                    isLoading ? <h2>Loading......</h2> :
+                    err ? <h2 style={{color:'red'}}>{err}</h2> :
+                    users.map((userObj)=>{
+                        return (
+                            <div key={userObj.id} className="card">
+                                <a rel="noreferrer" href={userObj.html_url} target="_blank">
+                                    <img alt="head_portrait" src={userObj.avatar_url} style={{width:'100px'}}/>
+                                </a>
+                                <p className="card-text">{userObj.login}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+}
