843468a7a23c92758f2ce021389a28a9c082de92
fuwenhao <fuwenhao594@163.com>
2023-08-03 21:47:48 +0800

    add: route6&params

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import React from 'react'
-import {NavLink,Routes,Route, Navigate, useRoutes} from 'react-router-dom'
-import About from './pages/About'
-import Home from './pages/Home'
+import {NavLink,useRoutes} from 'react-router-dom'
 import routes from './routes'
 
-/**
- * 
- * @returns 
- */
-
 export default function App() {
-    function computedClassName({isActive}){
-		return isActive ? 'list-group-item atguigu' : 'list-group-item'
-    }
-    const element = useRoutes(routes)
+	//根据路由表生成对应的路由规则
+	const element = useRoutes(routes)
 	return (
 		<div>
 			<div className="row">
@@ -25,26 +16,15 @@ export default function App() {
 				<div className="col-xs-2 col-xs-offset-2">
 					<div className="list-group">
 						{/* 路由链接 */}
-                        {/* 高亮 */}
-                        <NavLink className={computedClassName} to={'/about'}>about-class</NavLink>
-                        <NavLink className={computedClassName} to={'/home'}>home-class</NavLink>
-
-                        <NavLink className={"list-group-item"} to={'/about'}>about</NavLink>
-                        <NavLink className={"list-group-item"} to={'/home'}>home</NavLink>
-
-						{/* <NavLink className="list-group-item" to="/about">About</NavLink>
-						<NavLink className="list-group-item" to="/home">Home</NavLink> */}
+						<NavLink className="list-group-item" to="/about">About</NavLink>
+						<NavLink className="list-group-item" to="/home">Home</NavLink>
 					</div>
 				</div>
 				<div className="col-xs-6">
 					<div className="panel">
 						<div className="panel-body">
 							{/* 注册路由 */}
-							
-
-                            {/* 注册路由表 */}
-                            {element}
-                            
+							{element}
 						</div>
 					</div>
 				</div>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 import About from '../pages/About'
 import Home from '../pages/Home'
+import Message from '../pages/Message'
+import News from '../pages/News'
+import Detail from '../pages/Detail'
 import {Navigate} from 'react-router-dom'
 
 export default [
@@ -9,7 +12,23 @@ export default [
 	},
 	{
 		path:'/home',
-		element:<Home/>
+		element:<Home/>,
+		children:[
+			{
+				path:'news',
+				element:<News/>
+			},
+			{
+				path:'message',
+				element:<Message/>,
+				children:[
+					{
+						path:'detail/:id/:title/:content',
+						element:<Detail/>
+					}
+				]
+			}
+		]
 	},
 	{
 		path:'/',
