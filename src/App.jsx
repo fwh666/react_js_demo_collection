4a5bea1c799fd813c92c6af2244f61ee1892114f
ext.fuwenhao3 <ext.fuwenhao3@jd.com>
2023-08-03 18:02:56 +0800

    add: exten

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React, { Component,Fragment } from 'react'
 // import Demo from './components/03_hooks'
-import Demo from './components/04_fragment'
+// import Demo from './components/04_fragment'
+import A from './components/05_context'
+// import Parent from './components/06_optimize'
+// import Parent from './components/7_renderProps'
+import Parent from './components/8_ErrorBoundary/Parent'
 
 export default class App extends Component {
 	render() {
 		return (
 			<Fragment>
-				<Demo/>
+				{/* <Demo/> */}
+				{/* <A></A> */}
+				<Parent></Parent>
 			</Fragment>
 		)
 	}
